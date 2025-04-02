61283322bf729d3c98f8b949880d3742ea397b46
Michael V <michael.vorontsov@wemapnav.com>
2025-04-02 16:22:19 +0100

    Initial Commit

---
+++ ContentModules/ContentModulesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContentModulesApp.swift
+//  ContentModules
+//
+//  Created by Michael Vorotnstov on 02/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct ContentModulesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ContentModulesTests/ContentModulesTests.swift
@@ -0,0 +1,16 @@
+//
+//  ContentModulesTests.swift
+//  ContentModulesTests
+//
+//  Created by Michael Vorotnstov on 02/04/2025.
+//
+
+import Testing
+
+struct ContentModulesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
